e2dda25cfe133f171d46ab9d247553f438b77f5e
Catherine Bergeron <bergeron.catherine@icloud.com>
2024-03-02 13:05:58 -0500

    add les combinaisons pour les animaux et du UI dans la lab scene

--- Assets/scriptAsteroide.cs
+++ Assets/scriptAsteroide.cs
@@ -148,18 +148,18 @@ public class scriptAsteroide : MonoBehaviour
         int colorRnd = random.Next(0, 100);
         if (colorRnd >= 50)
         {
-            GameValues.instance.addItem(color.Name,1);
+            GameValues.instance.addItem(color.Name,1, GameValues.instance.inventory);
         }
 
         int formeRnd = random.Next(0, 100);
         if (formeRnd >= 50)
         {
-            GameValues.instance.addItem(form,1);
+            GameValues.instance.addItem(form,1,GameValues.instance.inventory);
         }
         int motifRnd = random.Next(0, 100);
         if (motifRnd >= 50)
         {
-            GameValues.instance.addItem(motif,1);
+            GameValues.instance.addItem(motif,1,GameValues.instance.inventory);
         }
 
     }
@@ -170,6 +170,7 @@ public class scriptAsteroide : MonoBehaviour
         if (vie <= 0)
         {
             GameValues.instance.incrementNbGenes(1);
+            RunManager.enemiesCleared++;
             Destroy(this.gameObject);
         }
         
