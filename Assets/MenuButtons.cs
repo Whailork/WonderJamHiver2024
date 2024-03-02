9d7f01cea0e2789375bd39b0b84650a21c68a490
Catherine Bergeron <bergeron.catherine@icloud.com>
2024-03-01 23:12:14 -0500

    merge main resolve conflicts

--- Assets/MenuButtons.cs
+++ Assets/MenuButtons.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
-//using Color = System.Drawing.Color;
+using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
     public void Play()
     {
+        SceneManager.LoadScene("sceneShip");
     }
 
     public void QuitGame()
